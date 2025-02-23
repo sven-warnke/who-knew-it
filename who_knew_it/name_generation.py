b7cfe36309a58cfefae7bd541fb8714d94cb31fe
Sven Warnke <sven@ge-warnke.de>
2025-02-23 21:40:54 +0100

    add name generation

---
+++ who_knew_it/name_generation.py
@@ -0,0 +1,21 @@
+import pathlib
+import random
+
+
+NAME_FILES_DIR = pathlib.Path(__file__).parent / "name-generation"
+
+
+def big_related_words() -> list[str]:
+    with open(NAME_FILES_DIR / "big-related-words.txt") as f:
+        return f.read().splitlines()
+
+
+def wet_related_words() -> list[str]:
+    with open(NAME_FILES_DIR / "wet-related-words.txt") as f:
+        return f.read().splitlines()
+
+
+def generate_player_name() -> str:
+    big_word = random.choice(big_related_words())
+    wet_word = random.choice(wet_related_words())
+    return f"{big_word} {wet_word}"
