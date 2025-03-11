a9f0ae8da1f989ec473093561021cd199afabe50
Sven Warnke <sven@ge-warnke.de>
2025-03-11 21:29:27 +0100

    add uncommon nouns

---
+++ who_knew_it/dictionary/generate_csv.py
@@ -0,0 +1,36 @@
+import pathlib
+
+text_file = pathlib.Path(__file__).parent / "old-english-nouns.txt"
+
+
+def main():
+    with open(text_file) as f:
+        lines = f.read().splitlines()
+
+    with open("nouns.csv", "w") as f:
+        f.write("noun|definition\n")
+        old_english = None
+        noun = None
+        definition = None
+        for line in lines:
+            line = line.rstrip()
+            if line:
+                if old_english is None:
+                    if line.startswith("Old English"):
+                        old_english = line
+                elif noun is None:
+                    if "," in line:
+                        noun = line.split(",", 1)[0]
+                elif definition is None:
+                    definition = line
+                else:
+                    old_english = None
+                    noun = None
+                    definition = None
+            else:
+                if old_english and noun and definition:
+                    f.write(f"{noun}|{definition}\n")
+
+
+if __name__ == "__main__":
+    main()
